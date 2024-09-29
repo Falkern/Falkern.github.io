2112b84f3b36ffc4c1fdda6d311a4320b0f4ee57
FALKERN <alphashadowyt048@gmail.com>
2024-09-29 12:32:03 +0530

    Create submit_form.php

---
+++ submit_form.php
@@ -0,0 +1,11 @@
+/**
+* This is still a dummy file.
+* Still testing the form submission.
+*/
+
+<body>
+
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
+
+</body
